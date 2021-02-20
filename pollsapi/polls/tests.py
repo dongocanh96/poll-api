af85b5baf48fd83b6e164c5ca00a9dbb36c8380a
dongocanh96 <ngocanh61119962gmail.com>
2021-02-20 16:33:41 +0700

    add ci

--- pollsapi/polls/tests.py
+++ pollsapi/polls/tests.py
@@ -1,3 +1,4 @@
+from rest_framework.test import APIClient
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
@@ -8,6 +9,7 @@ from polls import apiviews
 
 class TestPoll(APITestCase):
     def setUp(self):
+        self.client = APIClient()
         self.factory = APIRequestFactory()
         self.view = apiviews.PollViewSet.as_view({'get': 'list'})
         self.uri = '/polls/'
@@ -30,6 +32,13 @@ class TestPoll(APITestCase):
             HTTP_AUTHORIZATION='Token {}'.format(self.token.key))
         request.user = self.user
         response = self.view(request)
+        self.assertEqual(response.status_code, 200,
+                         'Expected Response Code 200, received {0} instead.'
+                         .format(response.status_code))
+    
+    def test_list2(self):
+        self.client.login(username="test", password="test")
+        response = self.client.get(self.uri)
         self.assertEqual(response.status_code, 200,
                          'Expected Response Code 200, received {0} instead.'
                          .format(response.status_code))
